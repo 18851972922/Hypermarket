a99d450def117327f0fdf2c1d968468986df9804
csyyyyyyyyyy <591017335@qq.com>
2020-06-08 15:19:36 +0800

    123

--- js/classify.js
+++ js/classify.js
@@ -35,3 +35,38 @@ window.onscroll = function(){
 		classifyNav.className = "classify-nav"
 	}
 }
+
+//倒计时
+	var day = document.querySelector('.day'); //天数的黑色盒子
+    var hour = document.querySelector('.hour'); //小时的黑色盒子
+    var minute = document.querySelector('.minute'); //分钟的黑色盒子
+    var second = document.querySelector('.second'); //秒数的黑色盒子
+    var inputTime = +new Date('2020-6-8 19:00:00'); //返回用户输入时间总的毫秒数
+    countDown(); //先调用一次函数防止第一次刷新页面有空白
+    setInterval(countDown, 1000); //调用函数
+
+    function countDown(time) {
+      var nowTime = +new Date(); //返回当前时间总的毫秒数
+      var times = (inputTime - nowTime) / 1000; //times是剩余时间总的毫秒数
+
+      var d = parseInt(times / 60 / 60 / 24); //天
+      d = d < 10 ? '0' + d : d;
+      day.innerHTML = d; 
+
+      var h = parseInt(times / 60 / 60 % 24); //时
+      h = h < 10 ? '0' + h : h;
+      hour.innerHTML = h; 
+
+      var m = parseInt(times / 60 % 60); //分
+      m = m < 10 ? '0' + m : m;
+      minute.innerHTML = m; 
+
+      var s = parseInt(times % 60); //秒
+      s = s < 10 ? '0' + s : s;
+      second.innerHTML = s; 
+	  
+	  if (h == 0 & m == 0 && s == 0){
+		  document.getElementById("time").innerText = "秒杀结束"
+		  clearInterval(inter)
+	  }
+    }
